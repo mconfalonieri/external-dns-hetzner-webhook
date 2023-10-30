53716fe11d240a28a677c1685acb3b50e7b80c6e
Marco Confalonieri <marco@marcoconfalonieri.it>
2023-10-31 00:20:36 +0100

    Corrected tests

--- cmd/webhook/main_test.go
+++ cmd/webhook/main_test.go
@@ -12,11 +12,11 @@ func Test_ServerOptions_empty(t *testing.T) {
 	if err := env.Set(&s); err != nil {
 		t.Fail()
 	}
-	assert.DeepEqual(t, s.Hostname, "0.0.0.0")
+	assert.DeepEqual(t, s.Hostname, "0.0.0.0:8888")
 }
 
 func Test_ServerOptions_value(t *testing.T) {
-	const testAddress = "127.0.0.1"
+	const testAddress = "127.0.0.1:8888"
 	t.Setenv("SERVER_HOST", testAddress)
 	s := serverOptions{}
 	if err := env.Set(&s); err != nil {
