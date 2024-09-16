d1a4f4aba3f05939fd62354f872ab2cd9f2d8a7f
Marco Confalonieri <marco@marcoconfalonieri.it>
2024-09-16 22:33:43 +0200

    Disabled license check in script

--- scripts/release_precheck.sh
+++ scripts/release_precheck.sh
@@ -19,4 +19,12 @@ make fmt
 check_git_status
 
 make static-analysis
-make license-check
+
+# License check fails with:
+#
+#    save.go:125:9: cannot use func(src string) (bool, error) {…} (value of type
+#    func(src string) (bool, error)) as func(srcinfo fs.FileInfo, src string,
+#    dest string) (bool, error) value in struct literal
+#
+# Will report bug to Google. In the meantime the check is disabled.
+# make license-check
