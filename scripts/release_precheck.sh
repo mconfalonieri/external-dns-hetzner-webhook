0ef4e9e1621fc6931acc87044c509ae4467f9b36
Marco Confalonieri <marco@marcoconfalonieri.it>
2025-10-22 21:03:58 +0200

    Modified prerelease script

--- scripts/release_precheck.sh
+++ scripts/release_precheck.sh
@@ -3,23 +3,14 @@
 set -e # exit on first error
 
 function check_git_status() {
-if [[ -z $(git status --porcelain) ]]
-then
+if [[ -z $(git status --porcelain) ]]; then
   echo "ok"
 else
   echo "Found modified files:"
   git status --porcelain
   exit 1
 fi
-}
 
-if [[ -z "$GITHUB_HEAD_REF" ]]
-then
-  echo "Running locally"
-else
-  echo "Branch: $GITHUB_HEAD_REF"
-fi
-exit 1
 check_git_status
 go mod tidy
 make fmt
